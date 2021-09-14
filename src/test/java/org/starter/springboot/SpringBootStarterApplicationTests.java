60df692165055e5ea29b8e2b65979f8d4c1e8588
Diego Canales <dreg27@gmail.com>
2021-09-14 13:10:26 -0300

    initial commit

---
+++ src/test/java/org/starter/springboot/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.starter.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
